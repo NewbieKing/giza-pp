acac4308dc4bac35a63120cd1441b8448380eb65
Andrew Yuan <solinight@outlook.com>
2017-09-27 18:02:01 +0800

    Update hmm.h

--- GIZA++-v2/hmm.h
+++ GIZA++-v2/hmm.h
@@ -78,8 +78,8 @@ class hmm : public model2
   HMMNetwork *makeHMMNetwork(const Vector<WordIndex>& es,const Vector<WordIndex>&fs,bool doInit)const;
   friend class model3;
   //这里添加两个用于把table打印到文件的函数
-  void print_ttable(filename);
-  void print_atable(filename);
+  void print_ttable(const char *filename);
+  void print_atable(const char *filename);
 };
 
 #endif
