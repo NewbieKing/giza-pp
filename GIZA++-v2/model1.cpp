b0a7e3942c5326c135c791c04557b20a0a892626
Andrew Yuan <solinight@outlook.com>
2017-10-12 17:44:43 +0800

    Update model1.cpp

--- GIZA++-v2/model1.cpp
+++ GIZA++-v2/model1.cpp
@@ -96,7 +96,8 @@ int model1::em_with_tricks(int noIterations, /*Perplexity& perp, sentenceHandler
     //这里的testPerp,testHandler都是我们的基类report_info中的数据成员,他们都是指针类型,这样它们可以进行逻辑判断就说的清了
     //在main.cpp中：这里的testPerp是前面定义的全局变量所以它必不为NULL,而testCorpus则取决于我们在./GIZA++时是否传入-tc参数。
     if (testPerp && testHandler) // calculate test perplexity
-      em_loop(it,*testPerp, *testHandler, seedModel1, dump_files, test_alignfile.c_str(), dictionary, useDict, *testViterbiPerp, true);//dump_files为false 
+      em_loop(it,*testPerp, *testHandler, seedModel1, dump_files, test_alignfile.c_str(), dictionary, useDict, *testViterbiPerp, true);
+      //dump_files为false，且最后一个参数默认为false 
     if( errorsAL()<minErrors )
       {
 	minErrors=errorsAL();
