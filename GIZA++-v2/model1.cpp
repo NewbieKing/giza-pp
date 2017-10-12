2063a8ba7eb011e4c36455c7bef44e75499b4f07
Andrew Yuan <solinight@outlook.com>
2017-10-12 17:37:48 +0800

    Update model1.cpp

--- GIZA++-v2/model1.cpp
+++ GIZA++-v2/model1.cpp
@@ -91,6 +91,7 @@ int model1::em_with_tricks(int noIterations, /*Perplexity& perp, sentenceHandler
     alignfile = Prefix + ".A" + shortModelName + "." + number ;
     test_alignfile = Prefix +".tst.A" + shortModelName + "." + number ;
     initAL();
+//void initAL(){ALmissingVALI=ALtoomuchVALI=ALeventsMissingVALI=ALeventsToomuchVALI=ALmissingTEST=ALtoomuchTEST=ALeventsMissingTEST=ALeventsToomuchTEST=ALmissing=ALtoomuch=ALeventsMissing=ALeventsToomuch=0;}
     em_loop(it,perp, sHandler1, seedModel1, dump_files, alignfile.c_str(), dictionary, useDict, trainViterbiPerp); //dump_files为false，即不会把对应的table写到文件中去
     //这里的testPerp,testHandler都是我们的基类report_info中的数据成员,他们都是指针类型,这样它们可以进行逻辑判断就说的清了
     //在main.cpp中：这里的testPerp是前面定义的全局变量所以它必不为NULL,而testCorpus则取决于我们在./GIZA++时是否传入-tc参数。
