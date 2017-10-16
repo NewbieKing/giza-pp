c7f37874693636294bbe9e82aae60282d237c3f3
Andrew Yuan <solinight@outlook.com>
2017-10-16 09:56:08 +0800

    Update myassert.h

--- GIZA++-v2/myassert.h
+++ GIZA++-v2/myassert.h
@@ -6,7 +6,7 @@ void imyerror(int line,const char *file,const char *expression);
 #define iassert(expression) do {if (!(expression)) {imyerror(__LINE__,__FILE__,#expression);}} while (0)
 
 #
-#define massert(expr) do {} while(0)
+#define massert(expr) do {} while(0)  //所以我对这里的定义有些疑惑，这里貌似是什么都不做的样子
 
 #define vassert(expr) do {} while(0)
 
